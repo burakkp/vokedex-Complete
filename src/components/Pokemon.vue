8ef5bfaac5eb44520350dc0ea6bee9b71e88626b
Kucukparmaksiz <burak.kucukparmaksiz@capgemini.com>
2020-12-24 10:11:23 +0100

    first commit

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="pokemon">
+    <div v-for="pokemon in pokemons" :key="pokemon.index">
+      <h2>Name: {{ pokemon.name }}</h2>
+      <p>type: {{ pokemon.type }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pokemon",
+  props: {
+    pokemons: {
+      type: Array,
+      default: () => {
+        return { name: "", type: "" };
+      },
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,23 +7,147 @@
       <div class="tiny-balls bg-success rounded-circle mr-1 mt-4" />
     </b-row>
     <b-row>
+      <h1 v-if="fire" class="fire">Flareon is a fire Pokémon</h1>
+      <h1 v-else-if="water" class="water">Horea is a water Pokémon</h1>
+      <h1 v-else-if="grass" class="grass">Bellosom is a grass Pokémon</h1>
+      <h1 v-else>No type specified</h1>
+      <h1 v-show="fire">Visible in the DOM, but invisible to the eye</h1>
     </b-row>
-    <b-row>
-    </b-row>
+    <ul>
+      <li v-for="detail in details" :key="detail.index">
+        {{ detail }}
+      </li>
+    </ul>
+    <div v-for="pokemon in pokemons" :key="pokemon.index">
+      <h3>{{ pokemon.name }}</h3>
+      <p>{{ pokemon.type }}</p>
+    </div>
+
+    <p :id="name">Hellow {{ firstName }}</p>
+    <button v-on:click="aClickHandler('hellow')">my ugly button</button>
+    <button
+      type="submit"
+      @click.prevent="aClickHandler('whow thats alert ' + firstName)"
+    >
+      Hellow
+    </button>
+
+    <form @submit="formSubmit">
+      <input v-model="searchQuery" />
+      <button type="submit">Search</button>
+      <button type="submit" @click.prevent="resetForm">reset</button>
+    </form>
+
+    <div :class="{darkTheme: isDark}">
+      <h2
+        v-for="pokemon in pokemons"
+        :key="pokemon.index"
+        :class="pokemon.color"
+      >
+        {{ pokemon.name }}
+      </h2>
+    </div>
+    <button @click="isDark = !isDark">Enter Darkness</button>
+
+    <h2>
+      {{title}}
+    </h2>
+
+    <h2 v-for="pokemon in fieryPokemons" :key="pokemon.index">
+      {{ pokemon }}
+    </h2>
+
+    
+    <b-row> </b-row>
+
+    <Pokemon :pokemons="starters" />
   </b-container>
 </template>
 
 <script>
+
+import Pokemon from "../components/Pokemon";
+
 export default {
   name: "Home",
+  components: {
+    Pokemon
+  },
   data() {
     return {
+      firstName: "Burak",
+      fire: false,
+      water: false,
+      grass: true,
+      details: ["awesome", "fire", "dragon"],
+      pokemons: [
+        { name: "charmender", type: "fire", color: "red" },
+        { name: "squirtle", type: "water", color: "blue" },
+        { name: "bulbasaur", type: "grass", color: "green" },
+      ],
+      pokemonss: ["charmander", "squirtle", "bulbasaur"],
+      poke: {
+        name: "bulbasaur",
+        number: "001"
+      },
+      checked: true,
+      isDark: false,
+      searchQuery: "",
+      starters: [
+        { name: "charmander", type: "fire" },
+        { name: "squirtle", type: "water" },
+        { name: "bulbasaur", type: "grass" },
+      ]
     };
   },
+  methods: {
+    resetForm() {
+      this.searchQuery = "";
+    },
+    formSubmit() {
+      alert(`hi you want to search for: ${this.searchQuery}`);
+    },
+    isFiery(value){
+      return value.includes("bulb")
+    }
+  },
+  computed: {
+    title(){
+      return this.poke.name + " " + this.poke.number
+    },
+    fieryPokemons() {
+      return this.pokemonss.filter(this.isFiery)
+    }
+  }
 };
 </script>
 
 <style lang="scss">
+.darkTheme{
+  background-color: black;
+
+  h2{
+    color: black;
+  }
+}
+.fire {
+  color: red;
+}
+.water {
+  color: blue;
+}
+.grass {
+  color: green;
+}
+.red {
+  color: red;
+}
+.blue {
+  color: blue;
+}
+.green {
+  color: green;
+}
 #blue-ball {
   height: 70px;
   width: 70px;
